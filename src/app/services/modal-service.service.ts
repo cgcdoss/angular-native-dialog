7fd3fd82406e6f755e61bbc0deb24412eb2fff80
cgcdoss <gabi04061997@gmail.com>
2023-06-28 23:12:24 -0300

    feat: trocando ng-container por ng-content

--- src/app/services/modal-service.service.ts
+++ src/app/services/modal-service.service.ts
@@ -10,9 +10,14 @@ export class ModalService {
   private _appRef = inject(ApplicationRef);
 
   public show(component: any): void {
+    const componentRef = createComponent(component, {
+      environmentInjector: this._environmentInjector,
+    });
+    this._appRef.attachView(componentRef.hostView);
+
     const dialogRef = createComponent(DialogComponent, {
       environmentInjector: this._environmentInjector,
-      // hostElement: document.querySelector('app-root')!,
+      projectableNodes: [[componentRef.location.nativeElement]],
     });
 
     document.body.appendChild(dialogRef.location.nativeElement);
@@ -21,11 +26,11 @@ export class ModalService {
     this._appRef.attachView(dialogRef.hostView);
 
 
-    dialogRef.instance.vc.createComponent(component);
     dialogRef.instance.dialog.nativeElement.showModal();
     dialogRef.instance.dialog.nativeElement.onclose = () => {
       document.body.querySelector('app-dialog')?.remove();
       dialogRef.destroy();
+      componentRef.destroy();
     };
   }
 }
